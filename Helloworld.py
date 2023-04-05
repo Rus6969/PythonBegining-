b17070eb139193e9a3990dcec9e7f09126e38fb7
Rus6969 <russam4515@gmail.com>
2023-04-05 00:27:37 -0400

    nunbers in phyron

--- Helloworld.py
+++ Helloworld.py
@@ -1,3 +1,18 @@
 print('Ruslan')
 
+print(5+5)
+# division always returns Float
+print(6/2)
+
+# sum with float will return float
+print(6+2.0)
+
+print (round(1.2*2,2))
+
+#remimder
+print(4%2==0)
+
+# stepen
+print(2**4)
+
 
