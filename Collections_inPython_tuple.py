82b797fb91130d29a57758291bf84f5815650aaa
Rus6969 <russam4515@gmail.com>
2023-04-14 00:00:11 -0400

    Logic operators

--- Collections_inPython_tuple.py
+++ Collections_inPython_tuple.py
@@ -1,7 +1,26 @@
 # analog array
 # ARE USED TO PROTECT DATA
-words = ("book","bottle", "pan", "waffel",4,0)
-#can NOT be modified
+words = ("book", "bottle", "pan", "waffel", 4, 0)
+# can NOT be modified
 
 
-person=("Ruslan","SmTOV", 32)
+person = ("Ruslan", "SmTOV", 32)
+len(person)
+print(person[1])
+players = [("Lebron", 38, 3700000), ("Curry", 34, 2600000)]
+print(players[0])
+
+print(words[2])
+
+
+# named tuple to search by name not index
+
+from collections import namedtuple
+
+Player = namedtuple("Player", "name age points")
+players = [Player("Lebron", 38, 3700000), Player("Curry", 34, 2600000)]
+
+print(players[0].name)
+print(players[0].age)
+print(players[0].points)
+print(type(players[0].age))
