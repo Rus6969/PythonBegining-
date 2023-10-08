a4f0935e02eb5f44149088a23dfccf6ce9baa31e
Ruslan <russam4515@gmail.com>
2023-10-08 11:30:03 -0400

    day2_input_Function

--- 100 Days of Code: The Complete Python Pro/day1.py
+++ 100 Days of Code: The Complete Python Pro/day1.py
@@ -1,1 +1,1 @@
-print("my name is Ruslan\n Im 31 years old")
+print("my name is Ruslan\n Im 31 years old\n Im russell")
---
+++ 100 Days of Code: The Complete Python Pro/day2_input_Function .py
@@ -0,0 +1,3 @@
+print("What is your name?")
+# nested input 
+print("hello "+ input("what is your name?"))
