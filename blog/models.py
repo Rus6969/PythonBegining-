d592e38c040cfde3b993ff53c724b943a2d2337e
Ruslan <russam4515@gmail.com>
2024-02-12 20:36:38 -0500

    renamed

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+STATUS = ((0,'Draft'),(1,'PUBLISHED'))
+class Post(models.Model):
+    title = models.CharField(max_length = 200)
+    content = models.TextField()
+    date_created= models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(max_length=100,unique =True)
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    status = models.IntegerField(choices =STATUS,default=0)
